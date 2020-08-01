23ccabbde01485463b2dd5e97086fe86f27142e7
craftycram <github@craftycram.net>
2020-08-01 15:28:21 +0200

    removed serial broadcast

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ port.on('open', () => {
     parser.on('data', (line) => {
       console.log(`Arduino: ${line}`);
       if (line !== lastData) {
-        io.broadcast.to(ownRoom).emit('serialresponse', line);
+        // io.emit('serialresponse', line);
         lastData = line;
       }
     });
